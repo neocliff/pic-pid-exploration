fa7ac9be17b0ec6726fc07b7569b2690a01bb4e6
Cliff Williams <neocliff@mac.com>
2020-04-20 20:18:37 +0000

    wip: toy code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pic-pid-exploration
-Code to explore PIC/PID features on x86_32 and x86_64
+
+Code to explore PIC/PID features on x86_32 and x86_64 processors. These
+processors are used in our target systems.
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * file: main.c
+ * author: Cliff Williams
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[], char * envp[]);
+
+
+int main(int argc, char *argv[], char *envp[]){
+    printf("Hello, world!\n");
+    return(0);
+}
+
